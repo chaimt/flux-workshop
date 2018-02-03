d905d81cd172d50ee845976612fd97915a3fae06
chaimt <cyturel@gmail.com>
2018-02-03 21:55:38 +0200

    base

---
+++ src/main/java/com/turel/fluxworkshop/FluxWorkshopApplication.java
@@ -0,0 +1,12 @@
+package com.turel.fluxworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxWorkshopApplication.class, args);
+	}
+}
---
+++ src/test/java/com/turel/fluxworkshop/FluxWorkshopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.turel.fluxworkshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FluxWorkshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
